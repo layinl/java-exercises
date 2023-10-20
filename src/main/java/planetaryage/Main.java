1e4105c464bd0612eec08f8def4f34491f5c7433
layinl <55414324+layinl@users.noreply.github.com>
2023-10-20 17:09:01 -0300

    feat and doc: translated all text and added comment desc.
    translated all text to english and added the description of the commented out code

--- src/main/java/planetaryage/Main.java
+++ src/main/java/planetaryage/Main.java
@@ -9,24 +9,27 @@ public class Main {
 		double earthAge = s.nextDouble();
 		String planet = s.next();
 
-		/*switch (planet) {
-			case "Marte":
-				System.out.printf("Idade equivalente em Marte: %.2f anos", earthAge / 1.88);
-				break;
-			case "Venus":
-				System.out.printf("Idade equivalente em Venus: %.2f anos", earthAge / 0.62);
-				break;
-			case "Jupiter":
-				System.out.printf("Idade equivalente em Jupiter: %.2f anos", earthAge / 11.86);
-				break;
-			default:
-				System.out.println("Planeta invalido.");
-		}*/
+		/* 
+  	 * If you're using Java 13 or later, use this code instead:
+		switch (planet) {
+		case "Marte":
+			System.out.printf("Mars' equivalent age: %.2f years", earthAge / 1.88);
+			break;
+		case "Venus":
+			System.out.printf("Venus' equivalent age: %.2f years", earthAge / 0.62);
+			break;
+		case "Jupiter":
+			System.out.printf("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
+			break;
+		default:
+			System.out.println("Invalid planet.");
+		}
+		*/
 		String out = switch (planet) {
-			case "Marte" -> String.format("Idade equivalente em Marte: %.2f anos", earthAge / 1.88);
-			case "Venus" -> String.format("Idade equivalente em Venus: %.2f anos", earthAge / 0.62);
-			case "Jupiter" -> String.format("Idade equivalente em Jupiter: %.2f anos", earthAge / 11.86);
-			default -> "Planeta invalido.";
+			case "Marte" -> String.format("Mars' equivalent age: %.2f years", earthAge / 1.88);
+			case "Venus" -> String.format("Venus' equivalent age: %.2f years", earthAge / 0.62);
+			case "Jupiter" -> String.format("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
+			default -> "Invalid planet.";
 		};
 
 		System.out.println(out);
