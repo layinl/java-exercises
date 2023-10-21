2314df0f872df1285c718977663dff829b269b3e
layinl <layon.final008@gmail.com>
2023-10-20 23:36:33 -0300

    feat: refactored the output and added docs

--- src/main/java/planetaryage/Main.java
+++ src/main/java/planetaryage/Main.java
@@ -1,40 +1,55 @@
 package planetaryage;
 
+import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * <p>
+ *   This program converts <b>earth years</b> to a selected planet's equivalent age (Mars, Venus or Jupiter)
+ * </p>
+ * <p>
+ *  E.g. 100 years in Earth is approximately equal to
+ * </p>
+ * <li>53.19 years in Mars</li>
+ * <li>161.29 in Venus</li>
+ * <li>8.43 in Jupiter</li>
+ */
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		double earthAge = s.nextDouble();
-		String planet = s.next();
+		Scanner scan = new Scanner(System.in);
 
-		/* 
-  	 * If you're using Java 13 or later, use this code instead:
-		switch (planet) {
-		case "Marte":
-			System.out.printf("Mars' equivalent age: %.2f years", earthAge / 1.88);
-			break;
-		case "Venus":
-			System.out.printf("Venus' equivalent age: %.2f years", earthAge / 0.62);
-			break;
-		case "Jupiter":
-			System.out.printf("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
-			break;
-		default:
-			System.out.println("Invalid planet.");
+		System.out.println("Please input the value of earth years");
+		double earthAge = scan.nextDouble();
+		System.out.println("Please input the planet to estimate its equivalent age (Mars, Venus or Jupiter)");
+		String planet = scan.next();
+
+		System.out.println(
+			switch (planet.toLowerCase(Locale.ROOT)) {
+				case "mars" -> String.format("Mars' equivalent age: %.2f years", earthAge / 1.88);
+				case "venus" -> String.format("Venus' equivalent age: %.2f years", earthAge / 0.62);
+				case "jupiter" -> String.format("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
+				default -> "Invalid planet.";
+			}
+		);
+
+		// If you're using Java 13 or later, use this code instead:
+		/*
+		switch (planet.toLowerCase(Locale.ROOT)) {
+			case "mars":
+				System.out.printf("Mars' equivalent age: %.2f years", earthAge / 1.88);
+				break;
+			case "venus":
+				System.out.printf("Venus' equivalent age: %.2f years", earthAge / 0.62);
+				break;
+			case "jupiter":
+				System.out.printf("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
+				break;
+			default:
+				System.out.println("Invalid planet.");
 		}
 		*/
-		String out = switch (planet) {
-			case "Marte" -> String.format("Mars' equivalent age: %.2f years", earthAge / 1.88);
-			case "Venus" -> String.format("Venus' equivalent age: %.2f years", earthAge / 0.62);
-			case "Jupiter" -> String.format("Jupiter's equivalent age: %.2f years", earthAge / 11.86);
-			default -> "Invalid planet.";
-		};
-
-		System.out.println(out);
-
-		s.close();
+		scan.close();
 	}
 
 }
--- src/main/java/trafficmonitoringsystem/Main.java
+++ src/main/java/trafficmonitoringsystem/Main.java
@@ -17,11 +17,6 @@ public class Main {
 
 	private static String suggestAlternativeRoute(String trafficEvent) {
 		return switch (trafficEvent) {
-			/*
-				ToDo: Implement logic to suggest alternative routes based on traffic events
-        Here, you should add logic to determine alternative routes based on the provided traffic event.
-        Return a suitable suggestion or message
-			*/
 			case "Normal Traffic" -> "Normal traffic on the main route. Continue on it.";
 			case "Congestion" -> "Avoid the main highway; use the secondary road.";
 			case "Accident" -> "Main road blocked. Use the secondary route.";
