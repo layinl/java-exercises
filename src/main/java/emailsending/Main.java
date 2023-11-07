2f69910506f4013c00fd781e412155285c5d1693
layinl <layon.final008@gmail.com>
2023-11-07 13:20:16 -0300

    feat and docs: added print statements and docs

--- src/main/java/emailsending/Main.java
+++ src/main/java/emailsending/Main.java
@@ -1,19 +1,31 @@
 package emailsending;
 import java.util.Scanner;
 
+/**
+ * This program simulates an e-mail sending and receiving.
+ * <li>Send: Prints out the message with the receiver</li>
+ * <li>Receive: It only prints that the inbox is empty</li>
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("""
+			What do you want to do?
+			'Send' e-mail
+			'Receive' e-mail
+			""");
 		String  command = scanner.nextLine();
 
 		if ("Send".equalsIgnoreCase(command)) {
 			if (scanner.hasNextLine()) {
-				String recipient = scanner.nextLine();
+				System.out.println("Please input the receiver e-mail");
+				String receiver = scanner.nextLine();
 				if (scanner.hasNextLine()) {
+					System.out.println("Please input the message");
 					String message = scanner.nextLine();
-					sendEmail(recipient, message);
+					sendEmail(receiver, message);
 				} else {
 					System.out.println("Invalid Command.");
 				}
@@ -28,10 +40,20 @@ public class Main {
 
 	}
 
-	private static void sendEmail(String recipient, String message) {
-		System.out.println("Email sent to " + recipient + " successfully!");
+	/**
+	 * Prints the 'e-mail sent' message along with the receiver
+	 * @param receiver the receiver e-mail that will get
+	 *                 the message
+	 * @param message the e-mail body
+	 */
+	private static void sendEmail(String receiver, String message) {
+		System.out.println("Email sent to " + receiver + " successfully!\n" +
+			message);
 	}
 
+	/**
+	 * Only prints 'Empty inbox' for now
+	 */
 	private static void receiveEmail() {
 		System.out.println("Empty Inbox.");
 	}
