c33db6c38c37ab50555bbfabd55bc1f453b38586
layinl <layon.final008@gmail.com>
2023-11-07 13:17:59 -0300

    fix: changed the scan inputs

--- src/main/java/library/Main.java
+++ src/main/java/library/Main.java
@@ -15,7 +15,9 @@ import java.util.stream.Stream;
  *  <li>price</li>
  *  <li>quantity</li>
  * </ul>
- * It also prints the total value and the number of books that was bought
+ * It also prints the total value and the number of books that was
+ * bought.
+ * <br />
  * You may input as many books as you want
  * @see Book
  */
@@ -23,24 +25,25 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		String title, isContinue;
+		String title = "", isContinue;
 		double price, totalPrice = 0;
 		int quantity, totalQuantity = 0;
 		Set<Book> books = new HashSet<>();
 		//  Book: { title, price, quantity }
 		do {
 			System.out.println("Please input the book's title");
-			// Had to do the title scan this way because for some reason
-			// this f***ing method catches the new line separator from the
-			// previous input and skips the title input
-			title = new Scanner(System.in).nextLine();
+			title = scan.nextLine();
 			System.out.println("Please input the book's price");
 			price = scan.nextDouble();
 			System.out.println("Please input the quantity");
 			quantity = scan.nextInt();
 			books.add(new Book(title, price, quantity));
 			System.out.println("Do you want to add another book? Type Y to continue");
-			isContinue = scan.next();
+			// Had to add this scan because for some reason this
+			// f***ing method catches the new line separator from
+			// the previous input and skips the command
+			scan.nextLine();
+			isContinue = scan.nextLine();
 		} while (isContinue.equalsIgnoreCase("y"));
 
 		for(Book book : books) totalPrice += book.getFullPrice();
