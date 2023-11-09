9aa667cbe35ff1e483bcc0d9cd215167e74b1c33
layinl <layon.final008@gmail.com>
2023-11-09 18:40:24 -0300

    feat and refactor: renamed geekstore package, inserted messages and an input validation

--- src/main/java/meangrade/Main.java
+++ src/main/java/meangrade/Main.java
@@ -1,4 +1,4 @@
-package geekstore;
+package meangrade;
 
 import java.util.Scanner;
 
@@ -6,7 +6,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
+		System.out.println("Please input your grade, from 0 to 10");
 		double mean = s.nextDouble();
+		if (mean < 0 || mean > 10) {
+			System.out.println("Please input a valid grade mean");
+			return;
+		}
 		System.out.println(
 			(mean < 5)? "REP" :
 			(mean < 7)? "MED" : "APR"
