7efbb053ee143820c9f089bbdf6db00cf96001a6
layinl <layon.final008@gmail.com>
2023-11-10 22:00:53 -0300

    feat and docs: added informational outputs and javadocs

--- src/main/java/irelandexchange/Main.java
+++ src/main/java/irelandexchange/Main.java
@@ -2,17 +2,33 @@ package irelandexchange;
 
 import java.util.Scanner;
 
+/**
+ * This program calculates and returns your total income deducted from
+ * Income Tax related to the salary, plus your benefits.
+ * <ul>
+ *   <li>Less than R$ 1100.00: <b>5%</b></li>
+ *   <li>Between the latter and R$ 2500.00: <b>10%</b></li>
+ *   <li>More than R$ 2500.00: <b>15%</b></li>
+ * </ul>
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		double grossSalary = s.nextDouble();
-		double benefits = s.nextDouble();
-		double tax = (grossSalary > 2500)? grossSalary * 0.15
+		double grossSalary, benefits, incomeTax;
+		System.out.println("Please input your salary");
+		grossSalary = s.nextDouble();
+		System.out.println("Please input your total income from benefits");
+		benefits = s.nextDouble();
+		incomeTax = (grossSalary > 2500)? grossSalary * 0.15
 			: (grossSalary > 1100)? grossSalary * 0.1
 			: grossSalary * 0.05;
 
-		System.out.printf("%.2f", grossSalary - tax + benefits);
+		System.out.printf(
+			"Total income: %.2f",
+			grossSalary - incomeTax + benefits
+		);
 	}
 
 }
