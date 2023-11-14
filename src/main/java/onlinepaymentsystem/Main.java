71269b3caf669f24d4e1233fcbe7f5df092d82dd
layinl <layon.final008@gmail.com>
2023-11-14 18:48:19 -0300

    feat and docs: added cardFlag variable, informational output and docs

--- src/main/java/onlinepaymentsystem/Main.java
+++ src/main/java/onlinepaymentsystem/Main.java
@@ -1,41 +1,71 @@
 package onlinepaymentsystem;
 
+import java.util.Locale;
 import java.util.Scanner;
 
+/**
+ * The Online Payment System program simulates a basic online
+ * purchase transaction.
+ * <br/>
+ * It may be done by either Boleto (a Brazilian's
+ * <s>obsolete</s> payment method) or Credit Card.
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Please input total value");
 		double paymentAmount = scanner.nextDouble();
 		scanner.nextLine(); // Consume the pending newline
 
-		//String paymentType = scanner.nextLine();
-
-		// ToDo: Add payment processing logic for "boleto" and "credit_card" types
-
-		switch (scanner.nextLine()) {
+		// This will only work in Java 15 or newer
+		System.out.println("""
+			What will be the payment method?
+			 - Boleto
+			 - credit_card
+			""");
+		switch (scanner.nextLine().toLowerCase(Locale.ROOT)) {
 			case "boleto" -> processBoletoPayment();
 			case "credit_card" -> processCreditCardPayment(scanner);
 			default -> System.out.println("Unrecognized payment type.");
 		}
 	}
 
-
+	/**
+	 * Processes the Boleto payment (actually just prints
+	 * that it was succeded for now)
+	 */
 	private static void processBoletoPayment() {
-		System.out.println("The payment with boleto was processed successfully.");
+		System.out.println("The payment with boleto was processed" +
+			" successfully.");
 	}
 
+	/**
+	 * Processes the Credit Card payment, requesting card data.
+	 * <br />
+	 * Then, prints that the payment was successful for now.
+	 * @param scanner the System.in scanner to receive user input
+	 */
 	private static void processCreditCardPayment(Scanner scanner) {
-		String cardNumber = scanner.nextLine();
-		String expirationDate = scanner.nextLine();
-		String securityCode = scanner.nextLine();
-		String cardholderName = scanner.nextLine();
+		String cardNumber, cardFlag, expirationDate, securityCode,
+			cardholderName;
+		System.out.println("Please input the card number");
+		cardNumber = scanner.nextLine();
+		System.out.println("Please input its flag");
+		cardFlag = scanner.nextLine();
+		System.out.println("Please input the card's expiration date");
+		expirationDate = scanner.nextLine();
+		System.out.println("Please input the security code (CVC)");
+		securityCode = scanner.nextLine();
+		System.out.println("Please input the name on the card");
+		cardholderName = scanner.nextLine();
 
-		// TODO: add the credit card payment processing logic
 		// TODO: validate the card details and perform the actual payment
-
-		System.out.println("The payment with credit card was processed successfully.");
+		// Gonna do this sh*t later. I'm too lazy for now
+		System.out.println("The payment with credit card was processed " +
+			"successfully.");
 	}
 
 }
