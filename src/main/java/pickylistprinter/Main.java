8ba458fd455eaecbb87a3e09203416594c4f6634
layinl <layon.final008@gmail.com>
2023-12-11 23:04:06 -0300

    feat: create pickylistprinter

---
+++ src/main/java/pickylistprinter/Main.java
@@ -0,0 +1,27 @@
+package pickylistprinter;
+
+import java.util.Scanner;
+
+/**
+ * This programs receives 10 lines and outputs the second, seventh and
+ * ninth line.
+ * <br/>
+ * <br/>
+ * ...yeah
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String[] names = new String[10];
+
+		for(int i = 0; i < 10; i++)
+			names[i] = sc.nextLine();
+		System.out.println(
+			names[2] + '\n'
+			+ names[6] + '\n'
+			+ names[8] + '\n'
+		);
+	}
+
+}
