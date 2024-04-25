a86066ac0b587714b71991c16aa336f8b923630b
layinl <layon.costa@protonmail.com>
2024-04-25 00:49:28 -0300

    feat: add basic bank account functionality implement checkBalance,receiveValue, transferValue and exit

--- src/main/java/bankaccount/Main.java
+++ src/main/java/bankaccount/Main.java
@@ -1,12 +1,15 @@
 package bankaccount;
 
+import java.util.Scanner;
+
 public class Main {
 
-  private String name = "DarkLayin";
-  private String accountType = "Checking";
+  private final String name = "DarkLayin";
+  private final String accountType = "Checking";
   private Double balance = 2500.0;
 
   void main() {
+    Scanner scan = new Scanner(System.in);
     System.out.println(
       STR."""
       ***********************
@@ -18,6 +21,34 @@ public class Main {
       ***********************
       """
     );
+    do {
+      System.out.println(
+        """
+        Operations
+  
+        1. Check balance
+        2. Receive value
+        3. Transfer value
+        4. Exit
+  
+        Please input the desired option:
+        """
+      );
+      switch(scan.nextInt()) {
+        case 1 -> System.out.println("Current balance: " + checkBalance());
+        case 2 -> {
+          System.out.println("Please input the value you will receive:");
+          receiveValue(scan.nextDouble());
+        }
+        case 3 -> {
+          System.out.println("Please input the value you will transfer:");
+          transferValue(scan.nextDouble());
+        }
+        case 4 -> {
+          return;
+        }
+      }
+    } while (true);
   }
 
   private Double checkBalance() {
@@ -28,8 +59,9 @@ public class Main {
     balance += value;
   }
 
-  private void tranferValue(Double value) {
-    balance -= value;
+  private void transferValue(Double value) {
+    if (balance >= value) balance -= value;
+    else System.out.println("You don't have enough balance to do this transaction");
   }
 
 }
