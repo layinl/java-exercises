7d5e2801b49a3f8960f5391efa091cb0f83f053d
layinl <layon.costa@protonmail.com>
2024-04-26 22:32:27 -0300

    docs: document bankaccount

--- src/main/java/bankaccount/Main.java
+++ src/main/java/bankaccount/Main.java
@@ -2,6 +2,14 @@ package bankaccount;
 
 import java.util.Scanner;
 
+/**
+ * The Bank Account program simulates a bank account and its fundamental
+ * operations, like:
+ * <li>Check balance</li>
+ * <li>Receive value</li>
+ * <li>Transfer value</li>
+ * @version 0.1
+ */
 public class Main {
 
   private final String name = "DarkLayin";
@@ -51,14 +59,27 @@ public class Main {
     } while (true);
   }
 
+  /**
+   * Checks the account's total balance and returns its value
+   * @return balance
+   */
   private Double checkBalance() {
     return balance;
   }
 
+  /**
+   * Receives the specified value and add to the balance
+   * @param value the value to be received
+   */
   private void receiveValue(Double value) {
     balance += value;
   }
 
+  /**
+   * Transfers the specified value if the value does not exceed total
+   * balance and subtracts it
+   * @param value the value to be transfered
+   */
   private void transferValue(Double value) {
     if (balance >= value) balance -= value;
     else System.out.println("You don't have enough balance to do this transaction");
